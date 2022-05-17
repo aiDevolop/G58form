52ff95bece6fc3e5b0f8126cffa9a44a71bdf61d
Yenou Isaiah Ayomide <97550820+aiDevolop@users.noreply.github.com>
2022-05-17 12:04:45 +0100

    Added files to this repository

---
+++ Js/index.js
@@ -0,0 +1,29 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
+
+
+
+const togglePassword = document
+            .querySelector('#togglePassword');
+  
+        const password = document.querySelector('#password');
+  
+        togglePassword.addEventListener('click', () => {
+  
+            // Toggle the type attribute using
+            // getAttribure() method
+            const type = password
+                .getAttribute('type') === 'password' ?
+                'text' : 'password';
+                  
+            password.setAttribute('type', type);
+  
+            // Toggle the eye and bi-eye icon
+            this.classList.toggle('bi-eye');
+        });
+;
+
